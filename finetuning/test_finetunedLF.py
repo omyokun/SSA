605d43e37f1303352d46ec2ca7ac70946a95d844
Swarnadeep <sbhar8597@gmail.com>
2025-03-28 22:51:59 +0100

    Update test_finetunedLF.py

--- finetuning/test_finetunedLF.py
+++ finetuning/test_finetunedLF.py
@@ -20,12 +20,6 @@ def naive_prompt(example):
         '''
     return prompt
 
-# def gen_test_data(sigma=30,length=200,seed=42):
-#     # Set seeds for reproducibility
-#     torch.manual_seed(seed)
-#     np.random.seed(seed)
-#     nums = [round(num, 2) for num in np.random.normal(loc=0, scale=sigma, size=length)]
-#     return nums
 
 def alternate_lists(list1, list2):
     if not isinstance(list1, list) or not isinstance(list2, list):
@@ -33,14 +27,13 @@ def alternate_lists(list1, list2):
     return [x if x in list2 else x for pair in zip(list1, list2) for x in pair]
 
 def gen_test_data(sigma1=1, sigma2=1,seed=42):
-    # Set seeds for reproducibility
+    
     torch.manual_seed(seed)
     np.random.seed(seed)
-    #weights = [round(num, 2) for num in np.random.normal(loc=0, scale=sigma1, size=2)]
+    
     weights = [round(num, 2) for num in np.random.normal(loc=0, scale=sigma2, size=2)]
     a = weights[0]
     b = weights[1]
-    #xs = [round(num, 2) for num in np.random.normal(loc=0, scale=sigma1, size=40)]
     xs = [round(num, 2) for num in np.random.normal(loc=0, scale=sigma1, size=40)]
     # Calculate ys using list comprehension
     ys = [round(a * x + b, 2) for x in xs]
@@ -68,7 +61,6 @@ def load_finetuned_model():
     print("Fine tuned Model and tokenizer Loaded Locally !!")
 
     print("We are trying to get outputs for the following data: ")
-    #nums = [1,2,-1,3,4]
     key = 1
     outputs = {}
     # Calculate total iterations for the outer progress bar
@@ -77,16 +69,12 @@ def load_finetuned_model():
     total_sigma1 = len(range(1, maxsigma1, 1))
     total_sigma2 = len(range(1, maxsigma2, 1))
     total_iterations = total_sigma1 * total_sigma2
-    #with tqdm(total=total_iterations, desc="Overall Progress") as pbar:
     for sigma1 in tqdm(range(1,maxsigma1,1), desc="sigma1", total=total_sigma1):
         for sigma2 in tqdm(range(1,maxsigma2,1), desc="sigma2", total=total_sigma2):
             seed = 100*sigma1+sigma2
             input,g_output = gen_test_data(sigma1=sigma1,sigma2=sigma2,seed=seed)
-            #user_prompt = f"CONTEXT: {input}"
-            #pred_output = generate_llama_output(user_prompt)
-            #nums = gen_test_data(sigma=sigma,length=length,seed=seed)
+            
             prompt = naive_prompt(input)
-                #print(f"Prompt passed to the model: {prompt}")
             try:
                 model_inputs = tokenizer(prompt,return_tensors = "pt").to("cuda")
                 output = model.generate(**model_inputs , max_length = 10000, pad_token_id= tokenizer.eos_token_id,eos_token_id= tokenizer.eos_token_id)
@@ -111,9 +99,7 @@ def load_finetuned_model():
                 print(f"Error: {e}")
                 print("-"*100)
                     
-                    #continue
             key += 1
-            #pbar.update(1)
     print("Dumping to Output file")
     with open("/tmpdir/user_name/finetuning/data/output/outputs_LFtest_Finally.json", "w") as f:
         json.dump(outputs, f, indent=4)
